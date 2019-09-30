5c831c611df659f06c21193cfd7815ed9ec69011
TaylorAkin <taylorakin.ky@gmail.com>
2019-09-30 11:27:19 -0400

    some styling

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 var sum = 0;
 
+//what's being displayed 
 var display = 0;
+
 
 var numberInput = 0;
 
+//display integer being transferred into a variable
 var numberStore1 = 0;
 
 var numberStore2 = 0;
 
+//variable to store operator
 var operatorStore = " ";
 
 var btnTextArray = ['C', ' ', ' ', '/', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', ' ', '='];
@@ -16,26 +20,23 @@ var btnTextArray = ['C', ' ', ' ', '/', '7', '8', '9', 'x', '4', '5', '6', '-', 
 
 //UI 
 function calculator() {
-    var displayContainer = document.createElement("div")
-    displayContainer.id = 'dcontainerid';
-    displayContainer.className = "container border";
 
     var displayRow = document.createElement("div");
     displayRow.id = 'drowid';
-    displayRow.className = 'row justify-content-center d-flex flex-grow-1';
+    displayRow.className = 'row justify-content-center';
 
     var displayCol = document.createElement("div");
     displayCol.id = "dcolid";
-    displayRow.className = 'col ml-5';
+    displayRow.className = 'col-9 ml-5 mt-5';
     displayCol.innerHTML = display;
 
-    var btnBody = document.createElement("div")
-    btnBody.id = 'btnBodyid';
-    btnBody.className = "container";
+    var calcContainer = document.createElement("div")
+    calcContainer.id = 'calcContainerId';
+    calcContainer.className = "container mt-5";
 
     var btnRow = document.createElement("div");
     btnRow.id = 'btnRowid';
-    btnRow.className = 'row justify-content-center d-flex flex-grow-1';
+    btnRow.className = 'row justify-content-center d-flex flex-grow-1 mx-3 my-3';
 
 
 
@@ -46,7 +47,7 @@ function calculator() {
         btnText.addEventListener('click', clickEvent);
         //creatina a button text array globally, and then setting the btn text to each index which runs through the loop.
         btnText.id = btnTextArray[i];
-        btnText.setAttribute('class', 'col-3 border display-4 justify-content-center');
+        btnText.setAttribute('class', 'col-3 border rounded-circle circle display-4');
         btnText.innerHTML = btnTextArray[i];
 
         // btnTextArray.push(0);
@@ -57,17 +58,17 @@ function calculator() {
     }
 
     displayRow.appendChild(displayCol);
-    displayContainer.appendChild(displayRow);
-    btnBody.appendChild(btnRow);
-    document.getElementById('calculatorhtml').appendChild(displayContainer);
-    document.getElementById('calculatorhtml').appendChild(btnBody);
+    calcContainer.appendChild(displayRow);
+    calcContainer.appendChild(btnRow);
+    document.getElementById('calculatorhtml').appendChild(calcContainer);
 
 }
 
 
 //
 function clickEvent() {
-    //index of array 
+    //index of array set to a variable called keypressed
+
     keypressed = (this.id);
 
 
@@ -82,101 +83,71 @@ function clickEvent() {
             display = 0;
             sum = 0;
             numberInput = 0;
-
-
-
-            break;
-        //
-        case '0':
-        case '1':
-        case '2':
-        case '3':
-        case '4':
-        case '5':
-        case '6':
-        case '7':
-        case '8':
-        case '9':
-        case '.':
-
-
-            //if display shows 0 it gets rid of it
-            if (numberInput == 0) {
-
-                numberInput = " "
-
-            }
-
-            //then display is nothing plus the integer you select, and if not 0 keeps adding to display
-            numberInput += keypressed;
-
-            display = numberInput;
-
-
             break;
 
-
-        case '+':
-        case '-':
-        case 'x':
-        case '/':
-
-
+        case '+': case '-': case 'x': case '/':
+            // if a operator is hit and it isn't a emtpy string, which happens after first click, next click evaluates
             if (operatorStore != " ") {
                 evaluate()
             }
-
             //storing first integer into a new variable, and resetting display to 0.
             numberStore1 = numberInput;
-
             numberInput = 0;
-
             operatorStore = keypressed;
-
-
             break;
 
         case '=':
-
             evaluate();
+            break;
+
+        //set numbers to default. Default says other keyspressed=this.id not defined in cases, do this. 
+        default:
+            //if display shows 0 it gets rid of it
+            if (numberInput == 0) {
+                numberInput = " "
+            }
+
+            //then display is nothing plus the integer you select, and if not 0 keeps adding to display..999999
+            numberInput += keypressed;
+            display = numberInput;
             break;
 
     }
 
-    //set innerhtml to display the numberstore string
+    //set innerhtml to the display variable  
     dcolid.innerHTML = display;
-
 }
 
 function evaluate() {
 
     numberStore2 = numberInput;
 
-    if (operatorStore == "+") {
+
+if (operatorStore == "+") {
 
 
-        sum = Number(numberStore1) + Number(numberStore2);
-        display = Number(sum.toPrecision(12));
-    }
+    sum = Number(numberStore1) + Number(numberStore2);
+    display = Number(sum.toPrecision(12));
+}
 
-    else if (operatorStore == "-") {
+else if (operatorStore == "-") {
 
-        sum = Number(numberStore1) - Number(numberStore2);
-        display = Number(sum.toPrecision(12));
-    }
+    sum = Number(numberStore1) - Number(numberStore2);
+    display = Number(sum.toPrecision(12));
+}
 
-    else if (operatorStore == "x") {
-        sum = Number(numberStore1) * Number(numberStore2);
-        display = Number(sum.toPrecision(12));
-    }
+else if (operatorStore == "x") {
+    sum = Number(numberStore1) * Number(numberStore2);
+    display = Number(sum.toPrecision(12));
+}
 
-    else if (operatorStore == "/") {
-        sum = Number(numberStore1) / Number(numberStore2);
-        display = Number(sum.toPrecision(12));
-    }
+else if (operatorStore == "/") {
+    sum = Number(numberStore1) / Number(numberStore2);
+    display = Number(sum.toPrecision(12));
+}
 
 
-    numberInput = display;
+numberInput = display;
 
 }
 
